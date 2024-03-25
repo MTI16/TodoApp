867304f2294678875130812e2334b9d52e655bd2
Mathis Strohfeldt <Mathis.Strohfeldt@hotmail.com>
2024-03-26 00:29:02 +0100

    added input and styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ export default function App() {
   return(
     <>
       <header>
-        <div id="logo">Todo</div>
+        <div id="logo">Todo App</div>
       </header>
       <main>
-        <p className='text-6xl text-white'>Mathis</p>
-        <button className="btn btn-outline btn-success">Success</button>
+        <div className="container-input">
+          <input type="text" placeholder="Was muss ich machen.." className="input input-bordered w-full max-w-xs" />
+          <button className="btn btn-outline btn-success" id='addButton'>Add</button>
+        </div>
+        <div className="divider divider-neutral" id='divider'></div>
       </main>
     </>
   );
