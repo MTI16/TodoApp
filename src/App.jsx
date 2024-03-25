d5506cf92e550c94a9359ef69dd9f5ec517fcf01
Mathis Strohfeldt <Mathis.Strohfeldt@hotmail.com>
2024-03-26 00:55:38 +0100

    todos addable and removable

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import { generateUUID } from 'three/src/math/MathUtils';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState();
+
+  function addTodo() {
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
+  function removeTodo(todo) {
+    setTodos(prevtodos => prevtodos.filter(item => item !== todo));
+  }
 
   return(
     <>
@@ -11,10 +22,22 @@ export default function App() {
       </header>
       <main>
         <div className="container-input">
-          <input type="text" placeholder="Was muss ich machen.." className="input input-bordered w-full max-w-xs" />
-          <button className="btn btn-outline btn-success" id='addButton'>Add</button>
+          <input type="text" placeholder="Was muss ich machen.." className="input input-bordered w-full max-w-xs" 
+          value={input}
+          onChange={e => setInput(e.target.value)}/>
+          <button className="btn btn-outline btn-success" id='addButton' onClick={addTodo}>add</button>
         </div>
         <div className="divider divider-neutral" id='divider'></div>
+        <div className="container-todos">
+          {todos.map(item => {
+            return(
+              <div key={generateUUID()} className='todo'>
+                <p className='text-2xl'>{item}</p>
+                <button className="btn btn-outline btn-error" id='removeButton' onClick={() => removeTodo(item)}>remove</button>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </>
   );
